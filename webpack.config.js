737598c2925ed37b302d0406d34e876ffccd82dc
DaniilT048 <tis.daniil98@gmail.com>
2025-03-23 21:13:17 +0200

    added bable

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development'
-const isProd = !isDev
-console.log('IS DEV:', isDev)
-console.log('IS PROD:', isProd)
+
 
 module.exports = {
     entry: {
@@ -16,7 +14,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
+        filename: isDev ? '[name].bundle.js' : '[name].bundle.[contenthash].js' ,
     },
     plugins: [
         new HTMLWebpackPlugin({ template: './src/index.html' }),
@@ -30,7 +28,7 @@ module.exports = {
             ]
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].min.css'
+            filename: '[name].bundle.[contenthash].min.css'
         })
     ],
 
@@ -54,7 +52,17 @@ module.exports = {
             {
                 test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
                 type: 'asset/resource'
-            }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
         ]
     }
 };
